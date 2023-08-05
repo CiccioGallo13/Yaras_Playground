852980b7d59104f9b840d9575df7594a31c86721
Ciccio <francescogallo728@gmail.com>
2023-08-05 14:23:39 +0200

    grammar for strings part

--- yara_visualizer/src/parser/grammar.ts
+++ yara_visualizer/src/parser/grammar.ts
@@ -1,28 +1,38 @@
 
 const GRAMMAR = `
-
 Expression
-  = "rule" _ [_a-zA-Z][_\-$a-zA-Z0-9]* _ "{" _
-  	Meta? _ Strings? _ Condition _ "}"
-    
+  = "rule" RuleName "{" _ ("meta:"Meta)? _ ("strings:"Strings)? _ ("condition:"Condition) _ "}"
+
+RuleName
+	= __ [_a-zA-Z][_\-$a-zA-Z0-9]* _ { return text().trim() }
+
 Meta
-	= "meta:" _ KeyValuePair+ _
+	= _ KeyValuePair+ _ { return text().trim()}
     
 Strings
-	= "strings:" _ Variable+ _
+	= Variable+ 
     
 Condition
-	= "condition:" _ 
+	= "" _ 
   	
 
 Variable
-	= "$"[_a-zA-Z][_\-$a-zA-Z0-9]* _ "=" _ VariableBody
+	= _ "$"[_a-zA-Z][_\-$a-zA-Z0-9]* _ "=" _ VariableBody _n { return text().trim() }
     
 VariableBody
-	= HexString // TextString / Regex
-    
+	= HexString / TextString / Regex
+
+TextString
+	= "\"" (Escape / [^\"\\])* "\"" [ ]* StringModifier?
+
+Regex
+	= "/" RegularExpression "/"("is"/"i"/"s")? [ ]* RegexModifier?
+
 HexString
-	= "{" _ (HexByte __ / Jump __/ OrHex __)+ _ "}"
+	= "{" _ (HexByte __ / Jump __/ OrHex __)+ _ "}" [ ]* "private"?
+
+RegularExpression
+	= ""
 
 HexByte
 	= _ "~"?[?0-9ABCDEF].{2}
@@ -37,7 +47,7 @@ OrHex
    
 
 KeyValuePair
-	= [a-zA-Z0-9]+ _ "=" _ (Integer / String / "true" / "false") _
+	= [a-zA-Z0-9]+ _ "=" _ (Integer / String / "true" / "false") _n { return text() }
     
 String
 	= "\"" [^\"]* "\""
@@ -48,10 +58,36 @@ Integer "integer"
 NumberLower256
 	= ([2][5][0-6]) / ([2][0-4][0-9]) / ([1][0-9][0-9]) / [1-9][0-9] / [0-9]
 
+StringModifier
+	= ("nocase" ([ ]+!("xor")!("base64")!("base64wide")AllModifier[ ]*)*)
+    / ("wide" ([ ]+AllModifier[ ]*)*) / ("ascii" ([ ]+AllModifier[ ]*)*)
+    / ("xor" ([ ]+!("nocase")!("base64")!("base64wide")AllModifier[ ]*)*)
+    / ("base64" ([ ]+!("xor")!("nocase")!("fullword")AllModifier[ ]*)*)
+    / ("base64wide" ([ ]+!("xor")!("nocase")!("fullword")AllModifier[ ]*)*)
+    / ("fullword" ([ ]+!("base64")!("base64wide")AllModifier[ ]*)*)
+	/ ("private" ([ ]+AllModifier[ ]*)*)
+    
+RegexModifier
+	= ("nocase" ([ ]+ReModifier[ ]*)*)
+    / ("wide" ([ ]+ReModifier[ ]*)*) / ("ascii" ([ ]+ReModifier[ ]*)*)
+    / ("fullword" ([ ]+ReModifier[ ]*)*)
+	/ ("private" ([ ]+ReModifier[ ]*)*)
+    
+ReModifier
+	= ("nocase" / "ascii" / "wide" / "fullword" / "private")
+
+AllModifier
+	= ("nocase" / "ascii" / "wide" / "fullword" / "base64" / "base64wide" / "xor" / "private")
+
+Escape
+	= "\\\"" / "\\\\" / "\\t" / "\\n" / "\\xdd"
+
 _ "whitespace"
   = [ \t\n\r]*
   
 __ "atLeastOneWhitespace"
 	= [ ]+
-
+    
+_n "newline"
+	= ([ ]*[\n]+[ ]*)+
 `
